8267f0bd20b31db9d0bc21f3ebc8a754a97fddf3
Yashwatts <149696539+Yashwatts@users.noreply.github.com>
2025-04-16 03:26:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: 'https://expense-tracker-frontend-two-xi.vercel.app',
+  origin: 'https://expensevault.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
